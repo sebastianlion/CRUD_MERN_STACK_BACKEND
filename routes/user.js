609a5b7b37011198358232b6dd77815595997836
sebastianlion <sebastianlion12@gmail.com>
2024-05-31 09:47:10 -0500

    branch change from master to main

--- routes/user.js
+++ routes/user.js
@@ -26,68 +26,67 @@ router.get("/adduser", (req, res) => {
 // Add users
 router.post("/adduser", async (req, res) => {
 	try {
-	const newUser = new ModelUser({
-		name: req.body.name,
-		email: req.body.email,
-		phone: req.body.phone,
-		userId: req.body.userId,
-	});
+		const newUser = new ModelUser({
+			name: req.body.name,
+			email: req.body.email,
+			phone: req.body.phone,
+			userId: req.body.userId,
+		});
 		await newUser.save();
 		res.status(200).end("User has been saved correctly");
 	} catch (error) {
 		console.error("Error saving user: ", error.res.data);
-		res.status(500).end("Error saving user")
+		res.status(500).end("Error saving user");
 	}
 });
 
 // Get all users
 router.get("/getusers", async (req, res) => {
 	try {
-		const docs = await ModelUser.find()
-			res.send(docs)
-		
+		const docs = await ModelUser.find();
+		res.send(docs);
 	} catch (err) {
-		console.error("Error getting users: ", err)
-		res.status(500).end("Error getting users")
+		console.error("Error getting users: ", err);
+		res.status(500).end("Error getting users");
 	}
-})
+});
 
-// Get user
+// Get one user
 router.post("/getuser", async (req, res) => {
 	try {
 		const validId = req.body.userId;
-		const user = await ModelUser.find({userId: validId})
-		res.send(user)
+		const user = await ModelUser.find({ userId: validId });
+		res.send(user);
 	} catch (err) {
-		console.error("Error getting user:", err)
-		res.status(500).end("Error getting user")
+		console.error("Error getting user:", err);
+		res.status(500).end("Error getting user");
 	}
-})
+});
 
 // update user
-router.post("/updateuser", async(req, res) => {
+router.post("/updateuser", async (req, res) => {
 	try {
 		const updateUser = {
 			name: req.body.name,
 			email: req.body.email,
 			phone: req.body.phone,
-		}
-		await ModelUser.findOneAndUpdate({userId: req.body.userId} ,updateUser)
-		res.status(200).end("User has been update correctly")
+		};
+		await ModelUser.findOneAndUpdate({ userId: req.body.userId }, updateUser);
+		res.status(200).end("User has been update correctly");
 	} catch (err) {
-		console.error("Error updating user: ", err)
-		res.status(500).end("Error updating user")
+		console.error("Error updating user: ", err);
+		res.status(500).end("Error updating user");
 	}
-})
+});
 
 // delete user
 
-router.post("/deleteuser", async(req, res) => {
+router.post("/deleteuser", async (req, res) => {
 	try {
-		await ModelUser.findOneAndDelete({userId: req.body.userId})
-		res.status(200).end("User has been delete correctly")
+		await ModelUser.findOneAndDelete({ userId: req.body.userId });
+		res.status(200).end("User has been delete correctly");
 	} catch (err) {
-		console.error("Error deleting user: ", err)
-		res.status(500).end("Error deleting user")
+		console.error("Error deleting user: ", err);
+		res.status(500).end("Error deleting user");
 	}
-})
+});
